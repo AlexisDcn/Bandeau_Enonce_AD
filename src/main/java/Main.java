871b9792341742d5ca790b7522865798155fa26a
AlexisDechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-12-04 11:25:07 +0100

    Commit Ajout Classe Main.java pour Clignotant.java et Rotation.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import bandeau.Bandeau;
+
+public class Main {
+    public static void main(String[] args) {
+        Bandeau monBandeau = new Bandeau();
+        Scenario scenario = new Scenario(monBandeau);
+
+        scenario.ajouterEffet(new Clignotant(monBandeau, 5, 500));
+        scenario.ajouterEffet(new Rotation(monBandeau, 2, 100));
+
+        scenario.jouer();
+        monBandeau.close();
+    }
+}
