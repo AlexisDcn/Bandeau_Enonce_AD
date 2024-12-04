8952115077b116f67916a5a3c03feb180a2085dd
AlexisDechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-12-04 11:24:34 +0100

    Commit Ajout Classe Effet.java

---
+++ src/main/java/Effet.java
@@ -0,0 +1,10 @@
+import bandeau.Bandeau;
+
+public abstract class Effet {
+    protected Bandeau bandeau;
+
+    public Effet(Bandeau bandeau) {
+        this.bandeau = bandeau;
+    }
+    public abstract void jouer();
+}
