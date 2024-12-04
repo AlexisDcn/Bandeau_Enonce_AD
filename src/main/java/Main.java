6d7a6b617a67a751dee7e3e246a8e32cb66cb07b
AlexisDechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-12-04 12:05:21 +0100

    Commit Modifications Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,14 @@ public class Main {
         Bandeau monBandeau = new Bandeau();
         Scenario scenario = new Scenario(monBandeau);
 
+        monBandeau.setMessage("Bonjour tout le monde !");
+        scenario.ajouterEffet(new Clignotant(monBandeau, 3, 500));
         scenario.ajouterEffet(new Teletype(monBandeau, "Hello, World!", 200));
         scenario.ajouterEffet(new Clignotant(monBandeau, 5, 500));
+        scenario.ajouterEffet(new Teletype(monBandeau, "On se prépare !", 200));
         scenario.ajouterEffet(new Rotation(monBandeau, 2, 100));
+        scenario.ajouterEffet(new Teletype(monBandeau, "Voilà ! C'est terminé !", 200));
+        monBandeau.sleep(500);
 
         scenario.jouer();
         monBandeau.close();
